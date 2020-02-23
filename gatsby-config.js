f31247b19b2cfaffba72705807a1f1a2328901b8
Shun Kakinoki <shunkakinoki@gmail.com>
2020-02-24 07:19:36 +0900

    feat: 🎸 markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-134813374-12",
+        trackingId: "UA-134813374-5",
         anonymize: true,
       },
     },
